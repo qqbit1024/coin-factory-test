40451f3d5c39df42d50b480b737bc0b190749c2a
qqbit <pavlik.merkulov@gmail.com>
2024-07-26 17:00:46 +0200

    Add gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  // basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-  // assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-      basePath: '/coin-factory-test',
-    assetPrefix: '/coin-factory-test/'
+  basePath: '/coin-factory-test',
+  assetPrefix: '/coin-factory-test/'
 };
 
 export default nextConfig;
